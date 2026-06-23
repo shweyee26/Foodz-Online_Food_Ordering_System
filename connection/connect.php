93612da806c61890c641ee57c5b8ca6886ccc62e
Shwe Yee <shweyeewinlae26@gmail.com>
2026-06-23 13:00:33 +0630

    Update connect.php

--- connection/connect.php
+++ connection/connect.php
@@ -9,17 +9,17 @@ $port = getenv('DB_PORT');
 $username = getenv('DB_USERNAME');
 $password = getenv('DB_PASSWORD');
 $dbname = getenv('DB_NAME');
-$ca_cert = getenv('DB_SSL_CERT'); // SSL cert stored as text
+//$ca_cert = getenv('DB_SSL_CERT'); // SSL cert stored as text
 
 // Create a temporary file for the SSL certificate
-$cert_file = tempnam(sys_get_temp_dir(), 'cert_');
-file_put_contents($cert_file, $ca_cert);
+//$cert_file = tempnam(sys_get_temp_dir(), 'cert_');
+//file_put_contents($cert_file, $ca_cert);
 
 // Create connection
 $db = mysqli_init();
 
 // Enable SSL using the temporary certificate file
-mysqli_ssl_set($db, NULL, NULL, $cert_file, NULL, NULL);
+//mysqli_ssl_set($db, NULL, NULL, $cert_file, NULL, NULL);
 
 // Establish connection
 if (!mysqli_real_connect($db, $host, $username, $password, $dbname, $port)) {
